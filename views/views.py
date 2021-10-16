5ad9a991d0b182c00912eb0fa198fe69b6eaff53
Shubhas <nimtsubash@gmail.com>
2021-10-16 09:55:08 -0400

    minor changes

--- views/views.py
+++ views/views.py
@@ -3,20 +3,20 @@ from mongo.db_helper import MongoDB
 
 
 def index():
-    db = MongoDB()
-    print(db.connection_string)
-    print(db.database_name)
-    print(db.mongo_client)
-
-    # list databases
-    # print(db._db)
-    # for d in db.mongo_client.list_databases():
-    #     print(d)
-
-    collection = db.get_collection("user")
-    result = collection.find({})
-    for document in result:
-        print(document)
+    # db = MongoDB()
+    # print(db.connection_string)
+    # print(db.database_name)
+    # print(db.mongo_client)
+    #
+    # # list databases
+    # # print(db._db)
+    # # for d in db.mongo_client.list_databases():
+    # #     print(d)
+    #
+    # collection = db.get_collection("user")
+    # result = collection.find({})
+    # for document in result:
+    #     print(document)
     context = {
         'name': "subash"
     }
